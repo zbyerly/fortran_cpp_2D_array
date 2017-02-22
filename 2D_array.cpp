4db9fb304821b8460b2d02e6bc3f6669bb6874bb
Zach Byerly <zbyerly@gmail.com>
2017-02-22 04:40:22 -0600

    seems to be working!

--- 2D_array.cpp
+++ 2D_array.cpp
@@ -36,10 +36,16 @@ int main()
 	    std::cout << array_ptrs[i] << std::endl;
 	    double *dptr;
 	    dptr = (double *)array_ptrs[i];
+
+	    //std::cout << dptr[2] << std::endl;
+	    
 	    for (int j=0; j<length1darrays; j++)
 		{
-		    std::cout << "dptr[" << j << "] = " << dptr[j] << std::endl;
+		    //std::cout << "dptr[" << j << "] = " << dptr[j] << std::endl;
+		    std::cout << "array[" << i << "," << j << "] = " << dptr[j]
+			      << std::endl;
 		}
+	    
 	}
     
     FNAME(print_values)(&module_ptr);
--- fortran_function.F90
+++ fortran_function.F90
@@ -28,15 +28,19 @@ subroutine init_array(module_c_ptr, num1darrays_fromc, length1darrays_fromc)
   print*, "Fortran (init_array): module_here%length1darrays = ", module_here%length1darrays
 
   print*, "Fortran (init_array): allocating module_here"
-  allocate(module_here%array2d(module_here%num1darrays,module_here%length1darrays))
-!  allocate(arrays_c_ptrs(module_here%num1darrays))
+
+  ! Fortran should store this (1,1)(2,1)(3,1) (2,1)(2,2)(2,3)
+  ! so I should be able to get the memory address for (1,i) and get
+  ! the i array
+  allocate(module_here%array2d(module_here%length1darrays,module_here%num1darrays))
+!  allocate(module_here%array2d(module_here%num1darrays,module_here%length1darrays))
   
   ! get the c pointer to return to cpp
   module_c_ptr = C_LOC(module_here)
 
   print*, "Fortran (init_array): printing values"
-  do i=1,module_here%num1darrays
-     do j=1,module_here%length1darrays
+  do i=1,module_here%length1darrays
+     do j=1,module_here%num1darrays
         module_here%array2d(i,j) = i+j*2.5
         print*, "array2d[",i,"][",j,"] = ",module_here%array2d(i,j)
      end do
@@ -57,8 +61,11 @@ subroutine get_array_ptr(module_c_ptr,i,array_c_ptr)
   type (arraytest_type), pointer :: module_here
 
   call C_F_POINTER(module_c_ptr,module_here)
+
+  print*, "index i = ", i
   
-  array_c_ptr = C_LOC(module_here%array2d(i,1))
+!  array_c_ptr = C_LOC(module_here%array2d(i+1,1))
+  array_c_ptr = C_LOC(module_here%array2d(1,i+1))
 
 end subroutine get_array_ptr
 
@@ -78,13 +85,20 @@ subroutine print_values(module_c_ptr)
   write(*,*) "Fortran (print_values): Entering subroutine"
 
   print*, "Fortran (print_values): printing values"
-  do i=1,module_here%num1darrays
-     do j=1,module_here%length1darrays
-        module_here%array2d(i,j) = i+j*2.5
+  do i=1,module_here%length1darrays
+     do j=1,module_here%num1darrays
         print*, "array2d[",i,"][",j,"] = ",module_here%array2d(i,j)
      end do
-     print*, " "
   end do
+
+!  do i=1,module_here%length1darrays
+!     do j=1,module_here%num1darrays
+!        print*, "array2d[",j,"][",i,"] = ",module_here%array2d(j,i)
+!     end do
+!  end do
   
   
 end subroutine print_values
+
+! array2d( length, num)
+! array2d( num, length)
